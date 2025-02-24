1c7b32691ea56882761ebe649dc9724ef73bbd54
Gopal88155 <gopalthakur88155@gmail.com>
2025-02-24 13:19:30 +0530

    new changes

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -1,24 +1,17 @@
-import React, { useContext } from 'react'
-import ListItem from './ListItem'
-import FeedbackContext from '../context/FeedbackContext'
+import React, { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
+import ListItem from "./ListItem";
 
 const ListGroup = () => {
-
-  const {feedbacks} = useContext(FeedbackContext)
-
-
+  const { feedbacks } = useContext(FeedbackContext);
 
   return (
-
     <ul className="my-3 list-group">
       {feedbacks.map((feedback) => (
         <ListItem key={feedback.id} feedback={feedback} />
-      ))
-      }
+      ))}
     </ul>
+  );
+};
 
-
-  )
-}
-
-export default ListGroup
+export default ListGroup;
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,44 +1,45 @@
-import React, { useContext } from 'react'
-import FeedbackContext from '../context/FeedbackContext'
+import React, { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
 
-const ListItem = ({feedback}) => {
-
-  const {dispatch} = useContext(FeedbackContext)
+const ListItem = ({ feedback }) => {
+  const { dispatch } = useContext(FeedbackContext);
 
   const handleDelete = (id) => {
     dispatch({
-      type : "DELETE_FEEDBACK" ,
-      payload : id
-    })  
-  }
+      type: "DELETE_FEEDBACK",
+      payload: id,
+    });
+  };
 
   const handleEdit = (feedback) => {
-
     dispatch({
-      type : "EDIT_FEEDBACK" ,
-      payload : feedback ,
-    })
-
-
-  }
-
-
+      type: "EDIT_FEEDBACK",
+      payload: feedback,
+    });
+  };
 
   return (
-    
     <li className="list-group-item shadow-lg">
       <h1>Rating : {feedback.rating} </h1>
-      <p> Review :  {feedback.review}  </p>
+      <p> Review : {feedback.review} </p>
       <span className="float-end">
-        <button className="btn btn-warning btn-sm"
-          onClick={() =>handleEdit(feedback) }>Edit</button>
-        <button className="btn btn-danger btn-sm mx-2"
-        onClick={() =>handleDelete(feedback.id) }>Delete</button>
+        <button
+          className="btn btn-warning btn-sm"
+          onClick={() => handleEdit(feedback)}
+        >
+          Edit
+        </button>
+        <button
+          className="btn btn-danger btn-sm mx-2"
+          onClick={() => handleDelete(feedback.id)}
+        >
+          Delete
+        </button>
       </span>
     </li>
+  );
+};
+
+export default ListItem;
 
 
-  )
-}
-
-export default ListItem
